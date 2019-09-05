3702543b108290d680d5c66f38862dcc6efa2726
Jalal Hejazi <jalal.hejazi@superusers.dk>
2019-09-05 14:57:07 +0200

    remove azure monitor

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var pkg = require('../package');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: pkg.name, version : pkg.version });
 });
 
 module.exports = router;
